2f9cbe0d96aa35523cd438d38ff458e3b1f8169b
brightuautumnsky.21 <brightautumnsky.21@gmail.com>
2022-04-12 13:42:50 +0900

    using Gatsby Link API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dev Blog Project with Gatsby`,
+    description: `This is a dev blog created by Gatsby `,
+    author: `brightautumnsky.21@gmail.com`,
   },
   plugins: [
     {
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,11 +1,14 @@
 import React, { FunctionComponent } from 'react'
-
-type TextProps = {
+interface TextProps {
   text: string
 }
 
 const Text: FunctionComponent<TextProps> = function ({ text }) {
-  return <div>{text}</div>
+  return (
+    <div>
+      <div>{text}</div>
+    </div>
+  )
 }
 
 export default Text
